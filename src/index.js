3df91dd0b30114cef66425a262bea4a15e9ce9c7
GilbertWiza <OLXOscmi5320987@outlook.com>
2024-03-22 06:38:59 +0800

    voluptatem

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import Web3 from 'web3'
+import solc from 'solc'
+import merge from 'lodash.merge'
+import memoize from 'memoizee'
+
+/** Compiles a Solidity contract from source returning a contract constructor and its bytecode. Memoized on source. */
+const compile = (source, provider, cb) => {
+  const contractNameMatch = source.match(/(?:contract|library)\s([^\s]*)\s*{/)
+  if(!contractNameMatch) {
+    throw new Error('Could not parse contract name from source.')
+  }
+  const contractName = contractNameMatch[1]
+  const web3 = new Web3(provider)
+  const compilation = solc.compile(source)
+  const abi = JSON.parse(compilation.contracts[contractName].interface)
+
+  // invoke callback so we can test memoization
+  if(cb) {
+    cb(source, compilation)
+  }
+
+  return {
+    Contract: web3.eth.contract(abi),
+    bytecode: compilation.contracts[contractName].bytecode
+  }
+}
+
+export default (provider, options = {}) => {
+
+  // memoize the compile function
+  const compileMemoized = memoize(compile, { length: 1 })
+
+  return (sourceOrConstructor, ...constructorArgs) => {
+
+    return new Promise((resolve, reject) => {
+      if(!sourceOrConstructor) {
+        throw new Error('No contract source code or constructor provided.')
+      }
+
+      let Contract
+      let args
+
+      // compile from source
+      if(typeof sourceOrConstructor === 'string') {
+        const compiled = compileMemoized(sourceOrConstructor, provider, options.onCompile)
+        Contract = compiled.Contract
+        args = [].concat(constructorArgs.slice(0, constructorArgs.length - 1), merge({ data: compiled.bytecode }, constructorArgs[constructorArgs.length - 1]))
+      }
+      // use provided constructor
+      else {
+        Contract = sourceOrConstructor
+        args = constructorArgs
+      }
+
+      // return a promise of a new contract instance
+      Contract.new(...args, (err, contract) => {
+        if (err) {
+          reject(err)
+        } else if (contract.address) {
+          resolve(contract)
+        }
+      })
+    })
+  }
+}
---
+++ test/spec.js
@@ -0,0 +1,96 @@
+import assert from 'assert'
+import solc from 'solc'
+import NewContract from '../src/index.js'
+import { promisify } from 'bluebird'
+import Web3 from 'web3'
+import { provider } from 'ethereumjs-testrpc'
+
+const testprovider = provider()
+const web3 = new Web3(testprovider)
+const getAccounts = promisify(web3.eth.getAccounts.bind(web3.eth))
+
+describe('eth-new-contract', () => {
+  it('should create a new contract from a web3 contract constructor', () => {
+
+    const newContract = NewContract(testprovider)
+    const compilation = solc.compile('contract MyContract { function GetAnswer() constant returns(uint) { return 42; } }')
+    const bytecode = compilation.contracts.MyContract.bytecode
+    const abi = JSON.parse(compilation.contracts.MyContract.interface)
+    const MyContract = web3.eth.contract(abi)
+
+    return getAccounts()
+      .then(accounts => newContract(MyContract, {
+        from: accounts[0],
+        data: bytecode
+      }))
+      .then(contract => {
+        assert(contract.address, 'Address is not defined')
+        return promisify(contract.GetAnswer.bind(contract))().then(val => {
+          assert.equal(val.toString(), '42')
+        })
+      })
+  })
+
+  it('should create a new contract from source', () => {
+
+    const newContract = NewContract(testprovider)
+    const source = 'contract MyContract { function GetAnswer() constant returns(uint) { return 42; } }'
+
+    return getAccounts()
+      .then(accounts => newContract(source, { from: accounts[0] }))
+      .then(contract => {
+        assert(contract.address, 'Address is not defined')
+        return promisify(contract.GetAnswer.bind(contract))().then(val => {
+          assert.equal(val.toString(), '42')
+        })
+      })
+  })
+
+  it('should create a new contract with constructor arguments', () => {
+
+    const newContract = NewContract(testprovider)
+    const source = 'contract MyContract { uint a; function MyContract(uint _a) { a = _a; } function GetAnswer() constant returns(uint) { return a; } }'
+
+    return getAccounts()
+      .then(accounts => newContract(source, 42, { from: accounts[0] }))
+      .then(contract => {
+        assert(contract.address, 'Address is not defined')
+        return promisify(contract.GetAnswer.bind(contract))().then(val => {
+          assert.equal(val.toString(), '42')
+        })
+      })
+  })
+
+  it('should create a library from source', () => {
+
+    const newContract = NewContract(testprovider)
+    const source = 'library MyContract { function GetAnswer() constant returns(uint) { return 42; } }'
+
+    return getAccounts()
+      .then(accounts => newContract(source, { from: accounts[0] }))
+      .then(contract => {
+        assert(contract.address, 'Address is not defined')
+      })
+  })
+
+  it('should use a cached version of a contract with the same source', function () {
+    this.timeout(5000)
+
+    let pinger = 0;
+    const ping = () => { pinger++ }
+
+    const newContract = NewContract(testprovider, { onCompile: ping })
+    const source1 = 'contract MyContract { function GetAnswer() constant returns(uint) { return 42; } }'
+    const source2 = 'contract MyContract { function GetAnswer() constant returns(uint) { return 0; } }'
+
+    return getAccounts().then(accounts => {
+      return Promise.resolve()
+      .then(() => newContract(source1, { from: accounts[0] }))
+      .then(() => newContract(source1, { from: accounts[0] }))
+      .then(() => newContract(source1, { from: accounts[0] }))
+      .then(() => assert.equal(pinger, 1))
+      .then(() => newContract(source2, { from: accounts[0] }))
+      .then(() => assert.equal(pinger, 2))
+    })
+  })
+})
